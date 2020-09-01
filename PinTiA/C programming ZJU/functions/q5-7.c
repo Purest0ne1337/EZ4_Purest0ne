367f2e3c02cf82a02eee07f9c87e67356497adb3
Purest0ne1337 <68551369+Purest0ne1337@users.noreply.github.com>
2020-09-01 20:08:00 +0800

    Create q5-7.c

---
+++ PinTiA/C programming ZJU/functions/q5-7.c
@@ -0,0 +1,13 @@
+double funcos(double e, double x) {
+    double sum = 1, elem = 1, fact = 2.0;
+    int i = 1;
+    while (elem >= e) {
+        elem = pow(x, (double)i * 2) / fact;
+        if (i % 2 == 0)  sum += elem;
+        else sum += -elem;
+        fact *= i * 2 + 1;
+        i++;
+        fact *= i * 2;
+    }
+    return sum;
+}
