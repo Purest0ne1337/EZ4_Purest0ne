8351b01cd09daa4a0f8593595442637b46457945
Purest0ne1337 <68551369+Purest0ne1337@users.noreply.github.com>
2020-11-24 22:38:17 +0800

    More mature

--- C++Programming/NJU Programming 5.8.13 custom_string.cpp
+++ C++Programming/NJU Programming 5.8.13 custom_string.cpp
@@ -2,15 +2,14 @@
 using namespace std;
 
 int my_strlen(const char* s) {
-    int count=0;
-    while (s[++count] != '\0') {
-    }
+    int count = -1;
+    while (s[++count]);
     return count;
 }
 
 void my_strcpy(char* new_string, const char* origin_string) {
     int i;
-    for (i = 0; origin_string[i] != '\0'; i++) {
+    for (i = 0; !origin_string[i]; i++) {
         new_string[i] = origin_string[i];
     }
     new_string[i] = '\0';
@@ -18,13 +17,12 @@ void my_strcpy(char* new_string, const char* origin_string) {
 
 void my_strncpy(char* new_string, const char* origin_string, int n) {
     int i;
-    for (i = 0; origin_string[i] != '\0' && i < n; i++) {
+    for (i = 0; !origin_string[i] && i < n; i++) {
         new_string[i] = origin_string[i];
     }
     new_string[i] = '\0';
 }
 
-//imitate Microsoft's code, Microsoft is a genius, but these ez 4 me.
 char* my_strcat(char* former, const char* latter) {
     char* p = former;
     while (*p) p++;
@@ -42,7 +40,7 @@ char* my_strncat(char* former, const char* latter, int n) {
 
 int my_strcmp(const char* s1, const char* s2) {
     while (*s1 == *s2) {
-        if ('\0' == *s1) return 0;
+        if (!*s1) return 0;
         s1++; s2++;
     }
     return *s1 - *s2;
@@ -50,8 +48,8 @@ int my_strcmp(const char* s1, const char* s2) {
 
 int my_strncmp(const char* s1, const char* s2, int n) {
     int i = 0;
-    while (i++ < n-1 && *s1 == *s2) {
-        if ('\0' == *s1) return 0;
+    while (i++ < n - 1 && *s1 == *s2) {
+        if (!*s1) return 0;
         s1++; s2++;
     }
     return *s1 - *s2;
